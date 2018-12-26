076b0c619a0fa7f0d5c469dc4669ec824e263f69
jcm300 <zeclmartins@gmail.com>
2018-12-26 00:27:11 +0000

    fix glusterFS, fix of haproxy with monitorization, separate prod version from test version

--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook playbook.yml
+if [ $# -eq 1 ]; then
+    if [ $1 = "p" ]; then 
+        ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook ./playbookProd.yml
+    elif [ $1 = "t" ]; then 
+        ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook ./playbookTest.yml
+    fi
+    if [ $1 != "p" ] && [ $1 != "t" ]; then
+        echo "Wrong Parameter"
+        echo "Please insert a paramenter:"
+        echo "p --> Production Deployment"
+        echo "t --> Test Deployment"
+    fi
+elif [ 1 ]; then 
+    echo "Please insert a paramenter:"
+    echo "p --> Production Deployment"
+    echo "t --> Test Deployment"
+fi
